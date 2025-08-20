177b36211b6b803ebc88832bdc3838e452cab5b4
AlbertMartorell <albert.martorell@opentrends.net>
2025-08-20 12:07:00 +0200

    Changes ColorScheme and improve how to display an image for the preview mode

--- app/src/main/java/com/martorell/albert/rickandmorty/ui/theme/Color.kt
+++ app/src/main/java/com/martorell/albert/rickandmorty/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Brown = Color(0xFFE4A788)
+val BrownDark = Color(0xFF44281D)
+val Green = Color(0xFF97CE4C)
